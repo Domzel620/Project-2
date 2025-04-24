8ab0aa4f91f3bebb8001031042f7012593c69825
Domzel620 <zelinsk2@go.stockton.edu>
2025-04-24 01:32:14 -0400

    Tested out smoother function

--- csvTester.java
+++ csvTester.java
@@ -9,6 +9,7 @@ public class csvTester {
         ArrayList<double[]> dataset = salt.dataReader("ProjectOutput\\plotOutPut.txt");
         ArrayList<double[]> parsedData = salt.salter(dataset);
         function.csvCreator(parsedData, "ProjectOutput\\newPlotOutPut");
-
+        ArrayList<double[]> smoothedData = salt.smoother(parsedData, 4);
+        function.csvCreator(smoothedData, "ProjectOutput\\smoothedDataOutPut");
     }
 }
