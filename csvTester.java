d5b4e672f983bc961872e2fa2efa48b31114a719
Domzel620 <zelinsk2@go.stockton.edu>
2025-04-23 20:34:40 -0400

    Fixed an issue in the tester

--- csvTester.java
+++ csvTester.java
@@ -5,7 +5,7 @@ public class csvTester {
     public static void main(String[] args) throws IOException {
         FunctionPlot function = new FunctionPlot();
         Salter salt = new Salter();
-        function.csvWriter(function.expFunc(0), "ProjectOutput\\plotOutPut.txt");
+        function.csvOverWriter(function.expFunc(0), "ProjectOutput\\plotOutPut.txt");
         ArrayList<double[]> dataset = salt.dataReader("ProjectOutput\\plotOutPut.txt");
         ArrayList<double[]> parsedData = salt.salter(dataset);
         function.csvCreator(parsedData, "ProjectOutput\\newPlotOutPut");
