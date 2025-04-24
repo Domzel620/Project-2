f8f0a9a735f7495147310616e254e042aafa3071
Domzel620 <zelinsk2@go.stockton.edu>
2025-04-24 01:28:02 -0400

    Created my Salter Function

--- Salter.java
+++ Salter.java
@@ -32,7 +32,7 @@ public class Salter {
         //This sets the range of the randomly salted y's to the biggest y value in the original dataset and 0;
         for(double[] numbas : dataset){
             double number = numbas[1];
-            
+
             if(number > bigNumba){
                 bigNumba = number;
             }
@@ -45,5 +45,48 @@ public class Salter {
             }
         }
         return dataset;
+    }
+
+    public ArrayList<double[]> smoother(ArrayList<double[]> dataset, int windowValue){
+        ArrayList<double[]> newSet = new ArrayList<>();
+        for(double[] coords : dataset){
+            double x = coords[0];
+            double y = coords[1];
+            newSet.add(new double[]{x,y});
+        }
+
+        int count = 0;
+        int length = dataset.size()-1;
+        for(double[] points : dataset){
+            double sum = 0;
+            int counter = 0;
+            int max = count+windowValue;
+            int min = count-windowValue;
+            if(min >= 0 && max <= length){
+                for(int i = min; i <= max; i++){
+                    sum += dataset.get(i)[1];
+                    counter++;
+                }
+            }else if(count >= windowValue && max > length){
+                for(int i = min; i <= length; i++){
+                    sum += dataset.get(i)[1];
+                    counter++;
+                }
+            }else if( min < 0 && max <= length){
+                for(int i = 0; i <= max; i++){
+                    sum += dataset.get(i)[1];
+                    counter++;
+                }
+            } else if(min < 0 && max > length){
+                for(int i = 0; i <= length; i++){
+                    sum += dataset.get(i)[1];
+                    counter++;
+                }
+            }
+            double average = sum/counter;
+            newSet.get(count)[1] = average;
+            count++;
+        }
+        return newSet;
     }
 }
