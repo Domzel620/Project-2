9da8c13d38a0ccf9ac3da451447e7c48dedcfc9a
Domzel620 <zelinsk2@go.stockton.edu>
2025-04-28 21:25:01 -0400

    Trying to do ANYTHING with jfreechart

---
+++ JavaPSSFIles/ChartCreator.java
@@ -0,0 +1,16 @@
+package JavaPSSFIles;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ChartCreator {
+    public void chartMaker(ArrayList<double[]> dataset,String title) {
+        System.out.println("Hello from ChartCreator!");
+    }
+}
--- JavaPSSFIles/csvTester.java
+++ JavaPSSFIles/csvTester.java
@@ -6,11 +6,11 @@ public class csvTester {
     public static void main(String[] args) throws IOException {
         FunctionPlot function = new FunctionPlot();
         Salter salt = new Salter();
-        function.csvOverWriter(function.expFunc(0), "ProjectOutput\\plotOutPut.txt");
-        ArrayList<double[]> dataset = salt.dataReader("ProjectOutput\\plotOutPut.txt");
+        function.csvOverWriter(function.expFunc(0), "JavaPSSFIles\\ProjectOutput\\plotOutPut.txt");
+        ArrayList<double[]> dataset = salt.dataReader("JavaPSSFIles\\ProjectOutput\\plotOutPut.txt");
         ArrayList<double[]> parsedData = salt.salter(dataset);
-        function.csvCreator(parsedData, "ProjectOutput\\newPlotOutPut");
+        function.csvCreator(parsedData, "JavaPSSFIles\\ProjectOutput\\newPlotOutPut");
         ArrayList<double[]> smoothedData = salt.smoother(parsedData, 4);
-        function.csvCreator(smoothedData, "ProjectOutput\\smoothedDataOutPut");
+        function.csvCreator(smoothedData, "JavaPSSFIles\\ProjectOutput\\smoothedDataOutPut");
     }
 }
